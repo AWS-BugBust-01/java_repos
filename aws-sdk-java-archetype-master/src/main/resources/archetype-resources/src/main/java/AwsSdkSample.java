93e3f59907971063cc25b9476e94e04368c2ae6b
Willem Visser <willem@gmail.com>
2021-11-08 11:17:07 -0800

    Update AwsSdkSample.java
    No real change this fixes the same bug as before

--- aws-sdk-java-archetype-master/src/main/resources/archetype-resources/src/main/java/AwsSdkSample.java
+++ aws-sdk-java-archetype-master/src/main/resources/archetype-resources/src/main/java/AwsSdkSample.java
@@ -1,9 +1,4 @@
 /*
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-*/
-/*
  * Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
