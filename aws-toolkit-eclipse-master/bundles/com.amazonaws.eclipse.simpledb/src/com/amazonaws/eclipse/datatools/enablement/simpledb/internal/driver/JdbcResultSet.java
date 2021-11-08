c83745a90420b6f22ebaedbb6cb849a0f888a3da
Pravin <pravin.mail@gmail.com>
2021-11-08 15:02:14 -0500

    Update JdbcResultSet.java

--- aws-toolkit-eclipse-master/bundles/com.amazonaws.eclipse.simpledb/src/com/amazonaws/eclipse/datatools/enablement/simpledb/internal/driver/JdbcResultSet.java
+++ aws-toolkit-eclipse-master/bundles/com.amazonaws.eclipse.simpledb/src/com/amazonaws/eclipse/datatools/enablement/simpledb/internal/driver/JdbcResultSet.java
@@ -265,9 +265,9 @@ public class JdbcResultSet implements ResultSet, ResultSetMetaData {
                 return null;
             }
             return new Date(new SimpleDateFormat().parse(str).getTime());
-        } catch (Exception e) {
-            throw new SQLException(e.getMessage());
-        }
+        } catch (Throwable t) {
+     throw new ServletException("Error: " + t.getMessage(), t);
+}
     }
 
     @Override
